057060a3598dbbaedc2353aaf901fe0d8dc2d71b
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-09 17:19:33 -0300

    Ok

--- src/cl/inacap/ejemplosplit/Start.java
+++ src/cl/inacap/ejemplosplit/Start.java
@@ -4,7 +4,7 @@ public class Start {
 
 	public static void main(String[] args) {
 		String textoUnido = "daniel@ejemplo.local";
-		String[] textoSeparado = textoUnido.split("@"); //El mÈtodo "split()", separa el string en un Array basandose en el par·metro entregado.
+		String[] textoSeparado = textoUnido.split("@"); //El m√©todo "split()", separa el string en un Array basandose en el par√°metro entregado.
 		
 		for (int i = 0; i < textoSeparado.length; i++) {
 			System.out.println(textoSeparado[i]);
